f2dfc5c042f11155f8c2a356edb11f57907f2976
Tzuikoniu <unitatoHD@yahoo.com>
2026-09-12 09:02:56 +0300

    Asteroids v1.0

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 from player import Player
-from constants import SCREEN_HEIGHT
-from constants import SCREEN_WIDTH
-from logger import log_state
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state, log_event
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
+from shot import Shot
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -18,11 +19,13 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
     asteroid_field = AsteroidField()
+    Shot.containers = (drawable, updatable, shots)
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
@@ -36,6 +39,18 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
