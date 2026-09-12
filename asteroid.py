bd34031cd7cb0a7019292b1b51667b8145ad582f
Tzuikoniu <unitatoHD@yahoo.com>
2026-09-12 15:18:35 +0900

    Asteroids Up

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
+import random
 
 
 class Asteroid(CircleShape):
@@ -12,3 +14,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            new_angle = random.uniform(20, 50)
+            a1_velocity= self.velocity.rotate(new_angle)
+            a2_velocity= self.velocity.rotate(-new_angle)
+            self.radius -= ASTEROID_MIN_RADIUS
+            a1 = Asteroid(self.position.x, self.position.y, self.radius)
+            a2 = Asteroid(self.position.x, self.position.y, self.radius)
+            a1.velocity = a1_velocity * 1.2
+            a2.velocity = a2_velocity * 1.2
--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 from circleshape import CircleShape
-from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 import pygame
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x: float, y: float) -> None:
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation = 0
+        self.rotation = 0.0
+        self.shoot_timer = 0.0
+
 
     def triangle(self) -> list[pygame.Vector2]:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -23,6 +26,7 @@ class Player(CircleShape):
 
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
+        self.shoot_timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -32,9 +36,22 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+
 
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
+        shot_instance = Shot(self.position.x, self.position.y)
+        shot_vector = pygame.Vector2(0, 1)
+        shot_rotated_vector = shot_vector.rotate(self.rotation)
+        shot_instance.velocity = shot_rotated_vector * PLAYER_SHOOT_SPEED
